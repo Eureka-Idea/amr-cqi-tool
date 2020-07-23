1413a42302f42815920ad2f8f5214c358fb14bea
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-07-23 17:13:16 -0400

    Updates

---
+++ precache-manifest.154f033a7c1e33cd188a3575e24ee072.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70fedb73dd0a571ec6a5e14ca4a5cf2",
+    "url": "/amr-cqi-tool/index.html"
+  },
+  {
+    "revision": "5f793295f63f3b21419c",
+    "url": "/amr-cqi-tool/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0ae6c4e381f546129d3a",
+    "url": "/amr-cqi-tool/static/css/main.22d60f6c.chunk.css"
+  },
+  {
+    "revision": "5f793295f63f3b21419c",
+    "url": "/amr-cqi-tool/static/js/2.3fd819b4.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/amr-cqi-tool/static/js/2.3fd819b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae6c4e381f546129d3a",
+    "url": "/amr-cqi-tool/static/js/main.386ee931.chunk.js"
+  },
+  {
+    "revision": "4ca44460120222b81a60",
+    "url": "/amr-cqi-tool/static/js/runtime-main.091189c8.js"
+  },
+  {
+    "revision": "1fd126e660dc0be5cb1fe1194c1047f2",
+    "url": "/amr-cqi-tool/static/media/africa-cdc-logo.1fd126e6.png"
+  },
+  {
+    "revision": "2ff30b5ddeaf9445b464770e02d9f70b",
+    "url": "/amr-cqi-tool/static/media/bd-logo.2ff30b5d.png"
+  },
+  {
+    "revision": "8ee6f9f881c72c4f97a90c589c08350f",
+    "url": "/amr-cqi-tool/static/media/dashboard.8ee6f9f8.png"
+  },
+  {
+    "revision": "58ca34befa5f380349b5673e39c1e0c3",
+    "url": "/amr-cqi-tool/static/media/find-logo.58ca34be.jpg"
+  }
+]);
